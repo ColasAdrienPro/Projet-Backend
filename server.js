ea93c956ad14a9367968bdd4c90428241bee83fd
ColasAdrienPro <colasadrienpro@gmail.com>
2026-06-02 10:46:54 +0200

    backend finish

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 const userRouter = require("./routes/userRouter")
 const recipeRouter = require("./routes/recipeRouter")
 const app = express()
 
 app.use(express.json());
+app.use(cors())
+
 
 app.use("/users",userRouter)
 app.use("/recipes",recipeRouter)
